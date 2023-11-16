29a101fcb568ce1a8504ee648bc3af66e763ea26
harringtonlyndsey <lyndsey.harrington@vodafone.com>
2023-11-16 14:19:02 +0000

    changes made

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -3,7 +3,7 @@ import React from 'react';
 export const Page = (props) => {
 
   return <>
-    <h3>{props.page.title}</h3>
+    <h3 onClick={({target: {innerText}}) => console.log("Value: ", innerText)}>{props.page.title}</h3>
   </>
 } 
 	
